0a9ef1dfa66a68005d3188237212e690db143e36
SLeBron <sophieclebron@gmail.com>
2022-08-15 16:43:33 -0600

    displayed current time and added table

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var moment = moment().format("MMM Do YYYY");
+var rows = document.getElementsByClassName("row");
 
 $("#currentDay").text(moment);
+
+if( === ) {
+    
+} else if( < ) {
+
+} else if( > )
+

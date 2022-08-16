e7b8e6e0d5ce8ad74250da60524a7b76e44b49d7
SLeBron <sophieclebron@gmail.com>
2022-08-15 18:56:24 -0600

    color coding table

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ var rows = document.getElementsByClassName("row");
 
 $("#currentDay").text(moment);
 
-if( === ) {
-    
-} else if( < ) {
+var currentTime = moment().format('MMM Do YYYY');
+var calendarHour = currentTime;
 
-} else if( > )
-
+function hourColor() {
+    if(calendarHour === currentTime) {
+        document.getElementsByClassName("table").style.backgroundColor = green;
+    } else if(calendarHour < currentTime) {
+        document.getElementsByClassName("table").style.backgroundColor = blue;
+    } else if(calendarHour > currentTime) {
+        document.getElementsByClassName("table").style.backgroundColor = red;
+    }
+}

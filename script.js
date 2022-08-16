1e1842551ce710acd928f2b397024012aa67cb98
SLeBron <sophieclebron@gmail.com>
2022-08-15 21:35:29 -0600

    got color coding to work and began event listener for user input

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-var moment = moment().format("MMM Do YYYY");
-var rows = document.getElementsByClassName("row");
+var myMoment = moment().format("MMM Do YYYY");
+var rows = document.getElementsByClassName("time-block");
 
-$("#currentDay").text(moment);
+$("#currentDay").text(myMoment);
 
-var currentTime = moment().format('MMM Do YYYY');
-var calendarHour = currentTime;
+// compare current time to hour of row, if same, if present, if future, then assign class 
 
-function hourColor() {
-    if(calendarHour === currentTime) {
-        document.getElementsByClassName("table").style.backgroundColor = green;
-    } else if(calendarHour < currentTime) {
-        document.getElementsByClassName("table").style.backgroundColor = blue;
-    } else if(calendarHour > currentTime) {
-        document.getElementsByClassName("table").style.backgroundColor = red;
-    }
-}
+function timeTracker() {
+    
+    let currentTime = moment().hour();
+    console.log(currentTime);
+    $(".time-block").each(function () {
+        var rowTime = parseInt($(this).attr("id").split("hour")[1]); 
+
+        if (currentTime === rowTime) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        } else if (currentTime < rowTime) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        } else (currentTime > rowTime) 
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        })
+    };
+timeTracker();
+// add event listener so when click time block can enter text
+var btn = document.getElementsByClassName("saveBtn col-1 btn");
+btn.addEventListener("click", function() {
+    
+});
+
+// store text in local storage
+
+// refresh page saved event persists
